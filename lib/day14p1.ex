95a91bd438de4a164dffbf93beb50e740aacd3ea
Karl Martin Teras <karl.martin@ter.as>
2021-12-14 08:29:18 +0200

    Clean up solution for day 14

--- lib/day14p1.ex
+++ lib/day14p1.ex
@@ -6,9 +6,6 @@ defmodule Day14P1 do
       |> String.trim()
       |> String.split("\n\n")
 
-    template = template
-               |> String.graphemes()
-
     rules =
       rules
       |> String.split("\n")
@@ -20,17 +17,14 @@ defmodule Day14P1 do
          )
       |> Map.new()
 
-    count =
-      insert(template, rules)
+    counts =
+      template
+      |> String.graphemes()
+      |> insert(rules)
       |> Enum.reduce(%{}, fn letter, map -> Map.update(map, letter, 1, fn ex -> ex + 1 end) end)
 
-    min_count =
-      count
-      |> Enum.reduce(100000000, fn {letter, count}, min_count -> min(count, min_count) end)
-
-    max_count =
-      count
-      |> Enum.reduce(0, fn {letter, count}, max_count -> max(count, max_count) end)
+    min_count = Enum.reduce(counts, 100000000, fn {_, count}, min_count -> min(count, min_count) end)
+    max_count = Enum.reduce(counts, 0, fn {_, count}, max_count -> max(count, max_count) end)
 
     max_count - min_count
   end
@@ -40,17 +34,17 @@ defmodule Day14P1 do
       template
     else
       {template, _} =
-        template
-        |> Enum.reduce(
-             {[], nil},
-             fn letter, {template, prev_letter} ->
-               if prev_letter != nil && Map.has_key?(rules, prev_letter <> letter) do
-                 {template ++ [Map.get(rules, prev_letter <> letter), letter], letter}
-               else
-                 {template ++ [letter], letter}
-               end
-             end
-           )
+        Enum.reduce(
+          template,
+          {[], nil},
+          fn letter, {template, prev_letter} ->
+            if prev_letter != nil && Map.has_key?(rules, prev_letter <> letter) do
+              {template ++ [Map.get(rules, prev_letter <> letter), letter], letter}
+            else
+              {template ++ [letter], letter}
+            end
+          end
+        )
       insert(template, rules, step + 1)
     end
   end
--- lib/day14p2.ex
+++ lib/day14p2.ex
@@ -18,7 +18,7 @@ defmodule Day14P2 do
              if prev_letter == nil do
                {map, letter}
              else
-               {Map.update(map, prev_letter <> letter, 1, fn ex -> ex + 1 end), letter}
+               {Map.update(map, prev_letter <> letter, 1, &(&1 + 1)), letter}
              end
            end
          )
@@ -27,101 +27,56 @@ defmodule Day14P2 do
       rules
       |> String.split("\n")
       |> Enum.map(
-           fn rule ->
-             [first, second] = String.split(rule, " -> ")
-             {first, second}
-           end
+           &String.split(&1, " -> ")
+            |> List.to_tuple()
          )
       |> Map.new()
 
-    {pair_map, first, last} = insert(pair_map, rules, String.slice(template, 0..1), String.slice(template, -2..-1))
-
-    count =
+    counts =
       pair_map
+      |> insert(rules)
       |> Enum.reduce(
            %{},
            fn {letters, count}, map ->
-
-             map = if letters == first do
-               Map.update(map, String.first(letters), count, fn ex -> ex + count end)
-             else
-               Map.update(map, String.first(letters), count, fn ex -> ex + count end)
-             end
-
-             map = if letters == last do
-               Map.update(map, String.last(letters), count, fn ex -> ex + count end)
-             else
-               Map.update(map, String.last(letters), count, fn ex -> ex + count end)
-             end
+             map
+             |> Map.update(String.first(letters), count, &(&1 + count))
+             |> Map.update(String.last(letters), count, &(&1 + count))
            end
          )
-      |> IO.inspect()
+      |> Map.update!(first_letter, &(&1 + 1))
+      |> Map.update!(last_letter, &(&1 + 1))
+      |> Map.map(&(elem(&1, 1) / 2))
+      |> Map.values()
 
-    min_count =
-      count
-      |> Enum.reduce(1000000000000000000000, fn {letter, count}, min_count ->
-        if letter == first_letter || letter == last_letter do
-          min((count + 1) / 2, min_count)
-        else
-          min(count / 2, min_count)
-        end
-      end)
-
-    max_count =
-      count
-      |> Enum.reduce(0, fn {letter, count}, max_count ->
-        if letter == first_letter || letter == last_letter do
-          max((count + 1) / 2, max_count)
-        else
-          max(count / 2, max_count)
-        end
-      end)
+    min_count = Enum.min(counts)
+    max_count = Enum.max(counts)
 
     max_count - min_count
   end
 
-  defp insert(pair_map, rules, first, last, step \\ 0) do
+  defp insert(pair_map, rules, step \\ 0) do
     if step == 40 do
-      {pair_map, first, last}
+      pair_map
     else
-      {pair_map, first, last} =
-        pair_map
-        |> Enum.reduce(
-             {pair_map, first, last},
-             fn {pair, count}, {pair_map, first, last} ->
-               #IO.inspect({pair, pair_map, Map.has_key?(rules, pair)})
+      pair_map
+      |> Enum.reduce(
+           pair_map,
+           fn {pair, count}, pair_map ->
+             if Map.has_key?(rules, pair) && count > 0 do
+               new_letter = Map.get(rules, pair)
+               left_pair = String.first(pair) <> new_letter
+               right_pair = new_letter <> String.last(pair)
 
-               if Map.has_key?(rules, pair) && count > 0 do
-                 new_letter = Map.get(rules, pair)
-                 left_pair = String.first(pair) <> new_letter
-                 right_pair = new_letter <> String.last(pair)
-
-                 pair_map =
-                   pair_map
-                   |> Map.update(pair, 0, fn ex -> ex - count end)
-                   |> Map.update(left_pair, count, fn ex -> ex + count end)
-                   |> Map.update(right_pair, count, fn ex -> ex + count end)
-
-                 first = if first == pair do
-                   left_pair
-                 else
-                   first
-                 end
-
-                 last = if last == pair do
-                   right_pair
-                 else
-                   last
-                 end
-
-                 {pair_map, first, last}
-               else
-                 {pair_map, first, last}
-               end
+               pair_map
+               |> Map.update(pair, 0, &(&1 - count))
+               |> Map.update(left_pair, count, &(&1 + count))
+               |> Map.update(right_pair, count, &(&1 + count))
+             else
+               pair_map
              end
-           )
-        |> IO.inspect()
-      insert(pair_map, rules, first, last, step + 1)
+           end
+         )
+      |> insert(rules, step + 1)
     end
   end
 end
